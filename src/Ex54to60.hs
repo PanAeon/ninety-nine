91ef20673b17694c938c01e034409416fa5860d5
PanAeon <vitalii.voloshyn@gmail.com>
2017-07-04 23:37:40 +0300

    teeeees

--- src/Ex54to60.hs
+++ src/Ex54to60.hs
@@ -33,13 +33,14 @@ insertElem (Branch x l r) = map (\l' ->
                                ) (insertElem r)
 
 
-prettyPrint :: Tree Char -> IO ()
+prettyPrint :: (Show a) => Tree a -> IO ()
 prettyPrint = prettyPrint' 0
 
-prettyPrint' :: Int -> Tree Char -> IO ()
+prettyPrint' ::(Show a) => Int -> Tree a -> IO ()
 prettyPrint' n Empty = putStrLn $ format' n "."
 prettyPrint' n (Branch x Empty Empty) = putStrLn (format' n $ show x)
-prettyPrint' n (Branch x l r) = putStrLn (format' n "(") >>= (\_ ->
+prettyPrint' n (Branch x l r) =
+   putStrLn (format' n "(") >>= (\_ ->
                                      putStrLn (format' n $ show x)) >>= (\_ ->
                                      prettyPrint' (n+1) l) >>= (\_ ->
                                      prettyPrint' (n+1) r
@@ -60,3 +61,51 @@ completelyBalancedTree' n = undefined
 
 rmdups :: (Ord a) => [a] -> [a]
 rmdups = map head . group . sort
+
+cbalTree :: Int -> [Tree Char]
+cbalTree 0 = [Empty]
+cbalTree n = let (q, r) = (n - 1) `quotRem` 2
+             in [q..q+r] >>= (\i ->
+                cbalTree i >>= (\l ->
+                  cbalTree (n - i - 1 ) >>= (\r ->
+                    return $ Branch 'x' l r
+                  )
+                )
+             )
+
+-- Problem 56  Symmetric binary trees
+symmetric :: Tree a -> Bool
+symmetric Empty = True
+symmetric (Branch x l r) = mirror l r
+
+mirror :: Tree a -> Tree a -> Bool
+mirror Empty Empty = True
+mirror (Branch xl ll rl) (Branch xr lr rr) =  (mirror ll rr) && (mirror rl lr)
+mirror _ _ = False
+
+-- Problem 57   Binary search trees (dictionaries)
+
+{-Use the predicate add/3, developed in chapter 4 of the course,
+ to write a predicate to construct a binary search tree from a list of integer numbers. -}
+
+buildTree :: [Int] -> Tree Int
+buildTree [] = Empty
+buildTree (x:xs) = insert x  (buildTree xs)
+                 where
+                   insert x Empty = Branch x Empty Empty
+                   insert x b@(Branch y l r) | x < y = Branch y (insert x l) r
+                                           | x > y = Branch y l (insert x r)
+                                           | x == y = b
+
+-- Problem 58 Generate-and-test paradigm
+
+{- Apply the generate-and-test paradigm to construct all symmetric,
+completely balanced binary trees with a given number of nodes. -}
+balancedSymmetricTree :: Int -> [Tree Char]
+balancedSymmetricTree 0 = []
+balancedSymmetricTree 1 = [leaf 'x']
+balancedSymmetricTree n = filter (\t -> isBalanced t && symmetric t)  ( balancedSymmetricTree (n - 2) >>= insertElem )
+                           where
+                             isBalanced Empty = True
+                             isBalanced (Branch _ l r) = isBalanced l && isBalanced r &&
+                                         abs ((maxLength l) - (maxLength r)) <= 1
