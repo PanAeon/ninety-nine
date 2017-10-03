66887c1c200acec08176af5a3b491a7d530b5935
PanAeon <vitalii.voloshyn@gmail.com>
2017-10-03 17:53:51 +0100

    more fixes

--- src/Lens/WikiTutorial.hs
+++ src/Lens/WikiTutorial.hs
@@ -1,10 +1,13 @@
-{-# LANGUAGE TemplateHaskell, RankNTypes #-}
+{-# LANGUAGE RankNTypes      #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Lens.WikiTutorial() where
 
 -- stack exec ghci
+--  :load WikiTutorial.hs
 -- reference: https://en.wikibooks.org/wiki/Haskell/Lenses_and_functional_references
-import Control.Lens
+import           Control.Lens
+import           Control.Lens.Setter
 
 data Point = Point {
     _positionX :: Double
@@ -14,7 +17,7 @@ makeLenses ''Point
 
 data Segment = Segment {
     _segmentStart :: Point
-  , _segmentEnd :: Point
+  , _segmentEnd   :: Point
 } deriving Show
 makeLenses ''Segment
 
@@ -31,3 +34,20 @@ foo = over (segmentEnd . positionY) (2*) testSeg
 
 foo'  = view (segmentEnd . positionY) testSeg
 foo'' = set  (segmentEnd . positionY) (-4.0) testSeg
+
+pointCoordinates:: Applicative f => (Double -> f Double) -> Point -> f Point
+pointCoordinates g (Point x y) = Point <$> g x <*> g y
+
+extremityCoordinates :: Applicative f => (Double -> f Double) -> Segment -> f Segment
+extremityCoordinates g (Segment a b) = Segment <$> g' a <*> g' b
+            where
+              g' = pointCoordinates g
+
+scaleSegment :: Double -> Segment -> Segment
+scaleSegment d = over extremityCoordinates (*d)
+
+ -- type Setter s t a b = forall f. Settable f => (a -> f b) -> s -> f t
+ -- (a -> f b) -> (f a) -> f (f b)
+mapped' :: Functor f => Setter (f a) (f b) a b
+mapped' = sets fmap -- :: ((a -> b) -> s -> t) -> Setter s t a b
+  --Identity $ fmap undefined ma
