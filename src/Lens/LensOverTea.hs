79d8da068d2048a0ac747aa73af269ddd74757c9
PanAeon <vitalii.voloshyn@gmail.com>
2017-10-07 23:15:24 +0100

    exercises

--- src/Lens/LensOverTea.hs
+++ src/Lens/LensOverTea.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes, TupleSections #-}
 module Lens.LensOverTea() where
 
 import Data.Bifunctor
@@ -17,7 +17,7 @@ import Data.Functor.Identity
 -- Is it really necessary to explicitly return the original value?
 -- Meet Storey, functor modifier const * functor
 type Lens s t a b = forall f . Functor f => (a -> f b) -> s -> f t
-type Lens' s a =  forall f .Functor f => (a-> f a) -> s -> f s
+type Lens' s a =  Lens s s a a
 
 data Storey x f a = Storey x (f a)
   deriving Show
@@ -40,5 +40,59 @@ ix index f list
 over :: Lens s t a b -> ((a -> b) -> s -> t)
 over l f = runIdentity . l (Identity . f)
 
+-- Test yourself --
+
+
+-- _1 :: Functor f => (a -> f b) -> (a, x) -> f (b, x)
+_1 :: Lens (a, x) (b, x) a b
+_1 f (a, x) = (,x) <$> f a
+
+-- _2 :: Functor f => (a -> f b) -> (x, a) -> f (x, b)
+_2 :: Lens (x, a) (x, b) a b
+_2  f (x, a) = (x,) <$> f a
+
+
+-- Make a lens out of a getter and a setter.
+lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
+lens get set f x = (set x) <$> (f (get x))
+
+
+-- Combine 2 lenses to make a lens which works on Either. (It's a good idea
+-- to try to use bimap for this, but it won't work, and you have to use
+-- explicit case-matching. Still a good idea, tho.)
+choosing :: Lens s1 t1 a b -> Lens s2 t2 a b
+         -> Lens (Either s1 s2) (Either t1 t2) a b
+choosing l1 l2 f s = case s of
+                     Left  s1 -> Left <$> l1 f s1
+                     Right s2 -> Right <$> l2 f s2  -- FIXME: what's that with bymap?
+
+choosing' :: Lens s1 t1 a b -> Lens s2 t2 a b
+         -> Lens (Either s1 s2) (Either t1 t2) a b
+choosing' l1 l2 f = either ((fmap Left) . l1 f) ((fmap Right ) . l2 f)
+
+
+
+-- Modify the target of a lens and return the result. (Bonus points if you
+-- do it without lambdas and defining new functions. There's also a hint
+-- before the end of the section, so don't scroll if you don't want it.)
+
+(<%~) :: Lens s t a b -> (a -> b) -> s -> (b, t)
+(<%~) l f s = (l (\x -> (f x, f x)) s)
+
+(<%~~) :: Lens s t a b -> (a -> b) -> s -> (b, t)
+(<%~~) l f s = (l ((,) <$> f <*> f) s)
+
+-- Modify the target of a lens, but return the old value.
+(<<%~) :: Lens s t a b -> (a -> b) -> s -> (a, t)
+(<<%~) l f s = (l ((,) <$> id <*> f) s)
+
+-- There's a () in every value. (No idea what this one is for, maybe it'll
+-- become clear later.)
+-- (a -> f b) -> s -> f t
+--  (() -> f ()) -> s -> f s
+united :: Lens' s ()
+united g s =  const s <$> (g ())
+
+
+
 -- TODO: other functions view, ...
--- TODO: Test yourself...
