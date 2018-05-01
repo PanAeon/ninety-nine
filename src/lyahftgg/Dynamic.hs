3c4a93ad7146639ddcd54c86fe51bcdfdb8470a0
PanAeon <vitalii.voloshyn@gmail.com>
2018-05-01 19:37:04 +0100

    foofofo

--- src/lyahftgg/Dynamic.hs
+++ src/lyahftgg/Dynamic.hs
@@ -1,20 +1,38 @@
 module Dynamic() where
 
+import Data.Array
+import Data.List(unfoldr)
 -- memoize?
 slow_ways :: (Int, Int) -> Integer
-slow_ways (1,_) = 1
-slow_ways (_,1) = 1
+slow_ways (0,_) = 1
+slow_ways (_,0) = 1
 slow_ways (i,j) = slow_ways (i-1,j) + slow_ways (i, j-1)
 
 -- https://wiki.haskell.org/Memoization
 
 -- my memoized is slower :)
-memoized_ways :: (Int, Int) -> Integer
-memoized_ways (r,c) = [ [ ways (i,j) | j <- [1..]] | i <- [1..] ] !! (r - 1) !! (c - 1)
+-- with arrays even slower..
+-- single list a little better then array :)
+-- pass mutable state inside??
+memoized_ways :: (Int, Int) -> (Int, Int) -> Integer
+memoized_ways (n,m) (r,c) =
+     [ ways (i,j) | i <- [0..n-1], j <- [0..m-1]] !! (r + n * c)
   where
-    ways (1,_) = 1
-    ways (_,1) = 1
-    ways (i,j) = memoized_ways (i-1,j) + memoized_ways (i, j-1)
+    ways (0,_) = 1
+    ways (_,0) = 1
+    ways (i,j) = memoized_ways (n,m) (i-1,j) + memoized_ways (n,m) (i, j-1)
+
+-- compute first row, and map that row..
+-- you need ,current row, and prev row
+-- wow, this is bloody fast!
+m_ways :: (Int, Int) -> Integer
+m_ways (n,m) = resultRow !! (m)
+  where
+    firstRow = [1 | i <- [0..m]]
+    resultRow = foldl f firstRow [0..n-1]
+    f prev _ = tail $ scanl (\p l -> p + l) (0) prev
+      --(head prev) : [ j | j <- [1..m-1]]
+
 
 slow_fib :: Int -> Integer
 slow_fib 0 = 0
